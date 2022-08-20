d9bbac76411251a487384a3e62917a8fe3398d12
AriseMarcher <2543061999@qq.com>
2022-08-20 11:41:21 +0800

    初始化封装axios请求

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,26 @@
 <template>
   <div id="app">
-    <div id="nav">
-      hello
-    </div>
+    <h1>hello vue2 & ts</h1>
     <router-view/>
   </div>
 </template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+import request from '@/utils/request'
+
+request({
+  method: 'GET',
+  url: '/boss/v2/api-docs?group=edu-boss-boot'
+}).then(res => {
+  console.log(res)
+})
+
+export default Vue.extend({
+  name: 'App'
+})
+</script>
 
 <style lang="scss">
 </style>
