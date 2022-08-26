72a603f073caf4c328b45ceea4ef3c1299444e69
AriseMarcher <2543061999@qq.com>
2022-08-26 22:52:36 +0800

    增加路由视图配置

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,14 +1,58 @@
 import Vue from 'vue'
 import VueRouter, { RouteConfig } from 'vue-router'
-import Home from './../views/home/index.vue'
 
 Vue.use(VueRouter)
 
 const routes: Array<RouteConfig> = [
   {
     path: '/',
-    name: 'Home',
-    component: Home
+    name: 'home',
+    component: () => import(/* webpackChunkName: 'home' */ '@/views/home/index.vue')
+  },
+  {
+    path: '/login',
+    name: 'login',
+    component: () => import(/* webpackChunkName: 'login' */ '@/views/login/index.vue')
+  },
+  {
+    path: '/role',
+    name: 'role',
+    component: () => import(/* webpackChunkName: 'role' */ '@/views/role/index.vue')
+  },
+  {
+    path: '/menu',
+    name: 'menu',
+    component: () => import(/* webpackChunkName: 'menu' */ '@/views/menu/index.vue')
+  },
+  {
+    path: '/resource',
+    name: 'resource',
+    component: () => import(/* webpackChunkName: 'resource' */ '@/views/resource/index.vue')
+  },
+  {
+    path: '/course',
+    name: 'course',
+    component: () => import(/* webpackChunkName: 'course' */ '@/views/course/index.vue')
+  },
+  {
+    path: '/user',
+    name: 'user',
+    component: () => import(/* webpackChunkName: 'user' */ '@/views/user/index.vue')
+  },
+  {
+    path: '/advert',
+    name: 'advert',
+    component: () => import(/* webpackChunkName: 'advert' */ '@/views/advert/index.vue')
+  },
+  {
+    path: '/advert-space',
+    name: 'advert-space',
+    component: () => import(/* webpackChunkName: 'advert-space' */ '@/views/advert-space/index.vue')
+  },
+  {
+    path: '*',
+    name: '404',
+    component: () => import(/* webpackChunkName: '404' */ '@/views/error-page/404.vue')
   }
 ]
 
